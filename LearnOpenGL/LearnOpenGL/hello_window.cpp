2eae61277eec948aa5811d3f413b6a4697648808
zh921 <18621621921@163.com>
2021-10-02 20:44:06 +0800

    update .

---
+++ LearnOpenGL/LearnOpenGL/hello_window.cpp
@@ -0,0 +1,80 @@
+﻿/*
+
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+
+using namespace std;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// 在GLFW中实现一些输入控制，这可以通过使用GLFW的几个输入函数来完成。
+// 我们将会使用GLFW的glfwGetKey函数，它需要一个窗口以及一个按键作为输入。
+// 这个函数将会返回这个按键是否正在被按下。我们将创建一个processInput函数来让所有的输入代码保持整洁。
+void processInput(GLFWwindow* window)
+{
+    // 检查用户是否按下"Esc"
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main()
+{
+
+    glfwInit(); // 初始化GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // 告诉GLFW我们要使用的OpenGL版本是3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 告诉GLFW我们使用的是核心模式(Core-profile)
+
+    // 创建一个窗口对象，这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到。
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // 在调用任何OpenGL的函数之前我们需要初始化GLAD。
+    // 给GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数。
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // 告诉OpenGL渲染窗口的尺寸大小，即视口(Viewport)，这样OpenGL才只能知道怎样根据窗口大小显示数据和坐标。
+    glViewport(0, 0, 800, 600);
+
+    // 对窗口注册一个回调函数(Callback Function)，它会在每次窗口大小被调整的时候被调用。
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行。
+    while (!glfwWindowShouldClose(window))
+    {
+        // 输入
+        processInput(window);
+
+        // 渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 检查并调用事件，交换缓冲
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+	return 0;
+}
+
+*/
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # LearnOpenGL
+
 https://learnopengl-cn.github.io
+
+## 软件配置
+
+VS2019
+
+https://www.glfw.org/download.html
+    glfw-3.3.4.zip
+
+https://cmake.org/download/
+    cmake-3.21.3-windows-x86_64.msi
+
+https://glad.dav1d.de/
+    4.6
+
+
