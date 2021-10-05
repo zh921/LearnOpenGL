1d163cc7564260915c88c1afa9868f7e19898a3f
zh921 <18621621921@163.com>
2021-10-05 08:43:33 +0800

    update .

--- LearnOpenGL/LearnOpenGL/hello_triangle_indexed.cpp
+++ LearnOpenGL/LearnOpenGL/hello_triangle_indexed.cpp
@@ -1,3 +1,4 @@
+/*
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -194,3 +195,4 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
+*/
